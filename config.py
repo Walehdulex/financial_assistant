a7c5dd6a5c254ed24e2e109e3dc8b15f1cb064d6
Walehdulex <dulexw@gmail.com>
2025-04-07 02:17:15 +0100

    Fixed requirements causing errors for deployment adn added a good database

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:yourpassword@localhost/financial_assistant")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:mKhZKiEoojnoLbYRnjfJiudjFoatnrAS@mysql.railway.internal:3306/railway")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ALPHA_VANTAGE_API_KEY = 'F3FVLPKT5JEIRH70'
     MAIL_SERVER = 'smtp.gmail.com'
@@ -13,3 +13,10 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    if os.getenv("FLASK_ENV") == "production":
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    else:
+         SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:mKhZKiEoojnoLbYRnjfJiudjFoatnrAS@mysql.railway.internal:3306/railway")
+        # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:yourpassword@localhost/financial_assistant'
+      
