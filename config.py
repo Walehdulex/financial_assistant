7943c93a9b5e1a4bda06d8fc3ff560b1e1b1137a
Walehdulex <dulexw@gmail.com>
2025-04-09 17:01:58 +0100

    Security updates

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
         SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://root:Dulex231@localhost/financial_assistant")  # For local or development
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ALPHA_VANTAGE_API_KEY = 'F3FVLPKT5JEIRH70'
+    ALPHA_VANTAGE_API_KEY =os.getenv("ALPHA_VANTAGE_API_KEY")
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
